25c09afb4e887e9853f1b45e3ce63643710b380d
Tufan Barış Yıldırım <tufanbarisyildirim@gmail.com>
2023-02-26 22:20:36 +0300

    remove building from check

--- internal/prometheus/metrics.go
+++ internal/prometheus/metrics.go
@@ -2,10 +2,12 @@ package prometheus
 
 import "github.com/prometheus/client_golang/prometheus"
 
+// Metrics all prometheus metrics
 type Metrics struct {
 	NodeRequests prometheus.Histogram
 }
 
+// NewMetrics Creates new metrics holder
 func NewMetrics() *Metrics {
 	return &Metrics{
 		NodeRequests: prometheus.NewHistogram(
